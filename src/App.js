a5dca680573780bc170d6b7debc41ca513a1d869
Omega Men <vanapasenko886@gmail.com>
2023-01-10 22:56:31 +0200

    create react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import s from './App.module.css';
+
+function App(props) {
+  
+  return (
+    <div className={s.container}>
+
+    </div>
+  );
+}
+
+export default App;
